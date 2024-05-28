800725661144c427abe172760fe3d404f575221a
Hassaan <jahangirhassan78@gmail.com>
2024-05-29 00:50:13 +0500

    step07b_const_enum

---
+++ step07b_const_enum/app.js
@@ -0,0 +1,9 @@
+"use strict";
+; //starts with 0
+var c = 1 /* Color.Green */;
+;
+var colorName = Color[3]; //Not allowed with const enums
+console.log(colorName);
+; //can assign values to all
+var colorIndex = 4 /* Color2["Blue"] */;
+console.log(colorIndex);
