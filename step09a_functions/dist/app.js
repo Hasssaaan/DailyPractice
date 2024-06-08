b084eb792dccc3ed888257a4e57b6ec13d05a365
Hassaan <jahangirhassan78@gmail.com>
2024-06-09 01:51:30 +0500

    step09_a functions

---
+++ step09a_functions/dist/app.js
@@ -0,0 +1,23 @@
+//Note: All parameters are required
+//Named function
+function add(x, y) {
+    return x + y;
+}
+//Anonymous function
+let myAdd1 = function (x, y) {
+    return x + y;
+};
+//Anonymous function with explict type
+let myAdd2 = function (x, y) {
+    return x + y;
+};
+//type names dont matter
+let myAdd3 = function (x, y) {
+    return x + y;
+};
+//Lambda functions															
+let myAdd4 = (a, b) => a + b;
+function greeter(fn) {
+    //....
+}
+export {};
